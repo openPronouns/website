bb56977876421c875a35907ca99a3b6680adf722
Ludoviko <me@ludoviko.ch>
2022-03-21 10:33:50 +0000

    About section is now fully translated!

--- js/lang-utils.js
+++ js/lang-utils.js
@@ -1,8 +1,13 @@
 const langs = ['en', 'eo'];
 
+document.addEventListener('DOMContentLoaded', () => {
+    activateLang('en');
+});
+
 function activateLang(lang) {
     setLang(lang);
     document.getElementById(lang + '-selector').className = 'active';
+    document.documentElement.lang = lang;
     for (let i = 0; i < langs.length; i++) {
         if (langs[i] != lang) {
             document.getElementById(langs[i] + '-selector').className = '';
